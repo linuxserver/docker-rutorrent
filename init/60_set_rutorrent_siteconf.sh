00c26f2ba2b9fdd459452c204a71fd51eaa854cc
sparklyballs <sparklyballs@gmail.com>
2016-01-24 16:35:07 +0000

    tidy up and fixing paths etc

--- init/60_set_rutorrent_siteconf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -f "/config/nginx/site-confs/default" ]; then
-rm /config/nginx/site-confs/default
-cp /defaults/rutorrent.www /config/nginx/site-confs/rutorrent
-fi
-chown abc:abc /config/nginx/site-confs/rutorrent
