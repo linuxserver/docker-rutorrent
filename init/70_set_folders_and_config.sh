e377a087b826363214c78dbd1fe6145c793a60ad
sparklyballs <sparklyballs@gmail.com>
2016-01-24 17:01:21 +0000

    fix missing ]

--- init/70_set_folders_and_config.sh
+++ init/70_set_folders_and_config.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /config/rtorrent/rtorrent_sess /config/log/rtorrent /detach_sess
 
-[[ ! -f /config/rtorrent/config.php ] && cp /defaults/config.php /config/rtorrent/config.php
+[[ ! -f /config/rtorrent/config.php ]] && cp /defaults/config.php /config/rtorrent/config.php
 [[ ! -L /config/www/webui/conf/config.php && -f /config/www/webui/conf/config.php ]] && rm /config/www/webui/conf/config.php
 [[ ! -L /config/www/webui/conf/config.php ]] && ln -s /config/rtorrent/config.php /config/www/webui/conf/config.php
 
