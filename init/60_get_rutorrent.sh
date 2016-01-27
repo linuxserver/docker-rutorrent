fa519809f6e1eb90ef92b49478337f1a030428cf
sparklyballs <sparklyballs@gmail.com>
2016-01-27 07:40:34 +0000

    tidying up and comment init files etc.

--- init/60_get_rutorrent.sh
+++ init/60_get_rutorrent.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# get webui
 [[ ! -d /config/www/webui/.git ]] && (git clone https://github.com/Novik/ruTorrent.git /config/www/webui && \
 chown -R abc:abc /config/www)
 
@@ -6,6 +8,7 @@ chown -R abc:abc /config/www)
 # opt out for autoupdates
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 
+# update webui
 cd /config/www/webui || exit
 git pull
 chown -R abc:abc /config/www
--- init/70_set_folders_and_config.sh
+++ init/70_set_folders_and_config.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# make folders
 mkdir -p /config/rtorrent/rtorrent_sess /config/log/rtorrent /detach_sess
 
+# copy config files/set links etc...
 [[ ! -f /config/rtorrent/config.php ]] && cp /defaults/config.php /config/rtorrent/config.php
 [[ ! -L /config/www/webui/conf/config.php && -f /config/www/webui/conf/config.php ]] && rm /config/www/webui/conf/config.php
 [[ ! -L /config/www/webui/conf/config.php ]] && ln -s /config/rtorrent/config.php /config/www/webui/conf/config.php
-
-
 [[ ! -f /config/rtorrent/rtorrent.rc ]] && cp /defaults/rtorrent.rc /config/rtorrent/rtorrent.rc
 
+# set perms
 chown abc:abc -R /config /detach_sess
 chown abc:abc /downloads
