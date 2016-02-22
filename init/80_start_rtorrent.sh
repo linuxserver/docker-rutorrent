c120ceb4b3adcd8d993a4d6dc919b28ab5837fe8
Stian Larsen <lonixx@gmail.com>
2016-02-22 20:37:28 +0100

    Mintor tweaks and removed script 80 from init.

--- init/80_start_rtorrent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# clear hanging .dtach file from previous session
-if [ -f "/detach_sess/.dtach" ]; then
-rm -f /detach_sess/.dtach || true
-sleep 1s
-fi
-
-dtach -n /detach_sess/.dtach /sbin/setuser abc /usr/bin/rtorrent -n -o import=/config/rtorrent/rtorrent.rc
